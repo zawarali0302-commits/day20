53600df748b7afa443b622ab53bfefd652c64efb
zawarali0302-commits <zawarali0302@gmail.com>
2025-11-15 20:01:29 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 
+import type React from 'react'
 import './App.css'
 import UserCardComponent from './components/UserCardComponent'
+
+interface ParentProps {
+  children: React.ReactNode
+}
+
+function Parent({ children }: ParentProps) {
+  return (
+    <div>
+      <h2>parent component</h2>
+      {children}
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
-     <UserCardComponent/>
+      <Parent>
+        <h1>Thsi is heading</h1>
+        <p>This is paragraph</p>
+      </Parent>
+      {/* <UserCardComponent/> */}
     </>
   )
 }
